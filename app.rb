36fe32e01b738ee3c7b146080111beeceae2340d
Drew Thompson <drewallynthompson@gmail.com>
2020-11-01 00:16:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name.capitalize}!"
+    "Hello #{@user_name}!"
   end
 
   get '/goodbye/:name' do
     @user_name = params[:name]
-    "Goodbye, #{@user_name}!"
+    "Goodbye, #{@user_name}."
   end
   
   get '/multiply/:num1/:num2' do
